1863275666b555050e1fcab72a43b4b8c47779f4
LeeSeungHun-35 <ish0542@naver.com>
2024-10-07 13:17:46 +0900

    수정

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -2,27 +2,23 @@ import React from 'react';
 import './Home.css';
 
 const Home = () => {
-  return (
-    <div>
-      <div className='Home1'>
-        <div className='Home1-content'>
-          <div className='title'>AI기반 딥페이크 판독 서비스</div>
-          <div className='content'>딥페이크: 진실을 왜곡하는 새로운 도구</div>
+    return (
+        <div>
+            <div className='Home1'>
+                <div className='Home1-content'>
+                    <div className='title'>AI기반 딥페이크 판독 서비스</div>
+                    <div className='content'>딥페이크: 진실을 왜곡하는 새로운 도구</div>
+                </div>
+            </div>
+            <div className='Home2'>
+                <div className='pic1' />
+                <div className='text1'>
+                딥페이크 기술은 인공지능을 활용하여 영상이나 이미지를 변형하는 방식. <br />특히, 특정 인물의 얼굴이나 목소리를 조작하여 허위 정보를 퍼뜨리거나 <br />동의 없이 음란물에 사용되는 등의 사례가 증가하고 있습니다. <br />이러한 문제를 해결하기 위해 AI 기반 탐지 기술과 법적 규제가 모색되고 있으며, 사회적 논의가 필요합니다 .
+                </div>
+            </div>
+            <div className='Home3'>Home333</div>
         </div>
-      </div>
-      <div className='Home2'>
-        <div className='Home2-content'>
-          <h2>현재 딥페이크로 인한 악용 사례</h2>
-          <p>
-            딥페이크 기술은 이미지와 비디오를 조작하여 가짜 정보를 생성할 수 있습니다. 이는
-            연예인의 이미지 도용, 정치적 목적의 가짜 뉴스 제작 등 여러 방식으로 악용될 수 있습니다.
-            특히, 이러한 기술은 사람들의 신뢰를 훼손하고 사회적 혼란을 초래할 수 있습니다.
-          </p>
-        </div>
-      </div>
-      <div className='Home3'>Home333</div>
-    </div>
-  );
+    );
 }
 
 export default Home;
