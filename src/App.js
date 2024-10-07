f103e5a0678c88a231f7f19535fcc7699445d746
LeeSeungHun-35 <ish0542@naver.com>
2024-10-07 11:46:11 +0900

    new file

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';  
+import Header from './Header/Header'; 
+import Home from './page/Home'; 
+import Test from './page/Test';  
+
+function App() {
+  return (
+    <div className="App"> 
+      <BrowserRouter>
+        <Header />  
+        <Routes>
+          <Route path="/" element={<Home />} />  
+          <Route path="/test" element={<Test />} />  
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/page/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div>
+      <div className='Home1'>
+        <div className='Home1-content'>
+          <div className='title'>AI기반 딥페이크 판독 서비스</div>
+          <div className='content'>딥페이크: 진실을 왜곡하는 새로운 도구</div>
+        </div>
+      </div>
+      <div className='Home2'>Home222</div>
+      <div className='Home3'>Home333</div>
+    </div>
+  );
+}
+
+export default Home;
