7aeff6b41a12a8e402aa55cebeb09a77b80f1f24
LeeSeungHun-35 <ish0542@naver.com>
2024-10-07 14:19:32 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';  
-import Header from './Header/Header'; 
-import Home from './page/Home'; 
-import Test from './page/Test';  
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Header/Header';
+import Home from './page/Home';
+import Test from './page/Test';
+
 
 function App() {
   return (
-    <div className="App"> 
+    <div className="App">
       <BrowserRouter>
-        <Header />  
+        <Header />
         <Routes>
-          <Route path="/" element={<Home />} />  
-          <Route path="/test" element={<Test />} />  
+          <Route path="/" element={<Home />} />
+          <Route path="/test" element={<Test />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -5,8 +5,8 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <nav>
-            <Link to="/">Home</Link>
-            <Link to="/Test">Test</Link>
+            <Link to="/">Home</Link>|
+            <Link to="/Test">판독하기</Link>
         </nav>
     );
 }
